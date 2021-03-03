be4cfc8b6ecb861aa8bb2663b1bc94362aad9df7
Samuel Vera <samuelvera1999@gmail.com>
2021-03-03 17:31:53 -0400

    Attribute binding lesson

--- app.js
+++ app.js
@@ -14,18 +14,28 @@ const vueConfig = {
           author: "Sewerslvt",
           date: new Date(2020, 11, 5).toLocaleDateString(),
           views: 103046,
+          bandcampUrl: "https://sewerslvt.com/album/the-world-is-fvcked",
+          alt: "The World is Fvcked",
+          imageUrl: "https://f4.bcbits.com/img/a1273865211_16.jpg",
         },
         {
           title: "Drowning In The Sewer",
           author: "Sewerslvt",
           date: new Date(2019, 5, 11).toLocaleDateString(),
           views: 488554,
+          bandcampUrl: "https://sewerslvt.com/album/drowning-in-the-sewer",
+          alt: "Drowning In The Sewer",
+          imageUrl: "https://f4.bcbits.com/img/a0515657606_16.jpg",
         },
         {
           title: "Everything Flows",
           author: "Tanpopo Crisis",
           date: new Date(2016, 7, 22).toLocaleDateString(),
           views: 167,
+          bandcampUrl:
+            "https://tanpopocrisis.bandcamp.com/album/everything-flows",
+          alt: "Everything Flows",
+          imageUrl: "https://f4.bcbits.com/img/a0092087838_16.jpg",
         },
       ],
     };
