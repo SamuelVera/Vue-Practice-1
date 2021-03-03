6e69ea2a31799f30a00258c4c47c0a9bea136af3
Samuel Vera <samuelvera1999@gmail.com>
2021-03-03 15:41:41 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+/** Config that will control Vue App */
+const vueConfig = {
+  /**Given template */
+  // template: "<h2>I am the template.</h2>", //Injected inside root
+  /**Data for Vue app */
+  data() {
+    return {
+      //Data accesible from Vue app scope
+      title: "The Final Empire",
+      author: "Brandon Sanderson",
+      age: 45,
+    };
+  },
+};
+//Instantiate the Vue App
+/**Vuew Application */
+const app = Vue.createApp({
+  //Data, functions
+  ...vueConfig,
+});
+
+//Mount Vue App with given selector as root
+app.mount("#root");
